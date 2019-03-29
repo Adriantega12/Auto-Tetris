1f8f6739be82873e643629a1026e5e10f5d2ebe0
Adrián Octavio Terrazas García <tavo_terrazasg@hotmail.com>
2019-03-29 15:00:52 -0600

    Main loop added to the game. Printing and cleaning screen constantly.

--- grid.py
+++ grid.py
@@ -1,18 +1,18 @@
-"""Grid class module. Basically the game board."""
+'''Grid class module. Basically the game board.'''
 
 
 class Grid:
-    """Grid class object to represent grid."""
+    '''Grid class object to represent the game board.'''
 
     def __init__(self, grid_width=10, grid_height=22):
-        """Init function."""
+        '''Init '''
         self._grid = [[0 for i in range(grid_width)] for j in range(grid_height)]
 
     def __getitem__(self, index):
-        """Access to grid using brackets as if it was just a matrix."""
+        '''Access to grid using brackets as if it was just a matrix.'''
         return self._grid[index]
 
     def print(self):
-        """Print grid to console."""
+        '''Print grid to console.'''
         for row in self._grid:
             print(row)
