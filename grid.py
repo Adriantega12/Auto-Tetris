bfa407ae5496cdd2d8f47e359bfb69636b4bdf0d
Adrián Octavio Terrazas García <tavo_terrazasg@hotmail.com>
2019-03-28 23:51:03 -0600

    Changed const values as constructor arguments with default values, just for flexibility in case it is needed for some reason

--- grid.py
+++ grid.py
@@ -4,12 +4,9 @@
 class Grid:
     """Grid class object to represent grid."""
 
-    GRID_HEIGHT = 22
-    GRID_WIDTH = 10
-
-    def __init__(self):
+    def __init__(self, grid_width=10, grid_height=22):
         """Init function."""
-        self._grid = [[0 for i in range(Grid.GRID_WIDTH)] for j in range(Grid.GRID_HEIGHT)]
+        self._grid = [[0 for i in range(grid_width)] for j in range(grid_height)]
 
     def __getitem__(self, index):
         """Access to grid using brackets as if it was just a matrix."""
