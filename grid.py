8709bf198befaae44e8b4f6a242ef153a766170c
Adrián Octavio Terrazas García <tavo_terrazasg@hotmail.com>
2019-03-28 23:13:42 -0600

    Lintered python code so far

--- grid.py
+++ grid.py
@@ -1,23 +1,21 @@
+"""Grid class module. Basically the game board."""
 
-GRID_HEIGHT = 22
-GRID_WIDTH = 10
 
 class Grid:
-  '''Class that represents the game board where pieces will fall and stack up'''
+    """Grid class object to represent grid."""
 
-  GRID_HEIGHT = 22
-  GRID_WIDTH = 10
+    GRID_HEIGHT = 22
+    GRID_WIDTH = 10
 
-  def __init__(self):
-    '''Init function'''
-    self._grid = [ [ 0 for i in range(GRID_WIDTH) ] for j in range(GRID_HEIGHT) ]
+    def __init__(self):
+        """Init function."""
+        self._grid = [[0 for i in range(Grid.GRID_WIDTH)] for j in range(Grid.GRID_HEIGHT)]
 
-  def __getitem__(self, index):
-    '''Indexing of grid to get grid rows or even individual cells using double indexing'''
-    return self._grid[index]
+    def __getitem__(self, index):
+        """Access to grid using brackets as if it was just a matrix."""
+        return self._grid[index]
 
-  def Print(self):
-    for row in self._grid:
-      print(row)
-
-g = Grid()
+    def print(self):
+        """Print grid to console."""
+        for row in self._grid:
+            print(row)
