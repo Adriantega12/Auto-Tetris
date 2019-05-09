09c7b01ffe5f590c5399dff4eed4480bbe2dd124
Adrián Octavio Terrazas García <tavo_terrazasg@hotmail.com>
2019-05-08 21:11:00 -0500

    AI interface has been written as well as a way to write inputs into a CSV file, and load the AI after training

--- neural_network.py
+++ neural_network.py
@@ -8,6 +8,7 @@ from sklearn import metrics
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
 
 data_frame = pd.read_csv('Data.csv')
 
@@ -20,7 +21,6 @@ print(data_frame.info())
 
 # MLP
 mlp_model = MLPClassifier(
-
     solver = 'lbfgs',
     )
 mlp_model.fit(x_train, y_train)
@@ -34,19 +34,33 @@ print('MLP Score: ')
 print('- Test: ', mlp_model.score(x_test, y_test))
 
 # SVC
-# svc_model = SVC()
-# svc_model.fit(x_train, y_train)
-# print(type(x_test[0]))
-# y_pred_svc = mlp_model.predict(x_test)
+svc_model = SVC()
+svc_model.fit(x_train, y_train)
+print(type(x_test[0]))
+y_pred_svc = mlp_model.predict(x_test)
 
-# print('Classification report:')
-# print(metrics.classification_report(y_test, y_pred_svc))
-# print('Confusion matrix:')
-# print(metrics.confusion_matrix(y_test, y_pred_svc))
-# print('SVC Score: ')
-# print('- Test: ', svc_model.score(x_test, y_test))
+print('Classification report:')
+print(metrics.classification_report(y_test, y_pred_svc))
+print('Confusion matrix:')
+print(metrics.confusion_matrix(y_test, y_pred_svc))
+print('SVC Score: ')
+print('- Test: ', svc_model.score(x_test, y_test))
+
+# Random Forest Classifier
+rfc_model = RandomForestClassifier()
+rfc_model.fit(x_train, y_train)
+print(type(x_test[0]))
+y_pred_rfc = mlp_model.predict(x_test)
+
+print('Classification report:')
+print(metrics.classification_report(y_test, y_pred_rfc))
+print('Confusion matrix:')
+print(metrics.confusion_matrix(y_test, y_pred_rfc))
+print('RFC Score: ')
+print('- Test: ', rfc_model.score(x_test, y_test))
 
 # Save files
 from joblib import dump
 dump(mlp_model, 'mlp.joblib')
-# dump(svc_model, 'svc.joblib')
+dump(svc_model, 'svc.joblib')
+dump(rfc_model, 'rfc.joblib')
